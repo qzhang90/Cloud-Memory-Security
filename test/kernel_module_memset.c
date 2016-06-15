f4667faf7a34d7568600851602fd88e4da16b27c
qzhang90 <qzhang90@gatech.edu>
2016-06-15 10:43:25 -0400

    Update kernel_module_memset.c

--- test/kernel_module_memset.c
+++ test/kernel_module_memset.c
@@ -1,6 +1,3 @@
-/*  
- *  hello.c - The simplest kernel module.
- */
 #include <linux/module.h>	/* Needed by all modules */
 #include <linux/kernel.h>	/* Needed for KERN_INFO */
 #include <linux/kthread.h>
