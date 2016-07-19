d7a0af132b409118b432236e03d02f4ad406793c
qzhang90 <qzhang90@gatech.edu>
2016-07-19 16:15:38 -0400

    Update test.c

--- kernel_probe/test.c
+++ kernel_probe/test.c
@@ -7,19 +7,28 @@
 #include <sys/user.h>
 #include <unistd.h>
 
-#define _syscall0(NR) stub_syscall0(NR)
+//#define _syscall1(NR) stub_syscall1(NR)
 #define __syscall_clobber "r11","rcx","memory"
 #define __syscall "syscall"
+
+#define MEMCLEAR	1
 long ret;
 
-unsigned long long getrsp()
-{
+unsigned long long getrsp(){
   __asm__ ("movq %rsp, %rax");
 }
 
 
 int f2(void){
 	int a, b, c;
+	unsigned long i;
+
+	register unsigned long rbp asm("rbp");
+
+	for(i = 0; i < 500; i++){
+		printf("%2lx ", *((char *)rbp + i)&0xFF);
+	}	
+	printf("\n\n\n");
 	
 	a = 1;
 	b = 2;
@@ -36,7 +45,7 @@ int f1(void){
 	int r1 = fwrite(buff, 1, 512, f1);
 	
 	fclose(f1);
-	int ret;
+	int ret, tmp = 123;
 	register unsigned long rbp asm("rbp");	
 	register unsigned long rsp asm("rsp");	
 
@@ -49,10 +58,13 @@ int f1(void){
 	*so that all EIP on the kernel stack will be the EIP of the caller (e.g. f1())
 	*/        
 	
+
+#ifdef MEMCLEAR
 	__asm__ volatile (__syscall
                 : "=a" (ret)
-                : "0" (316) : __syscall_clobber );
-
+                : "0" (316), "D" (tmp)
+		: __syscall_clobber );
+#endif
 	/*A malicious user resumes rbp/rsp after the system call*/	
 	//__asm__ ("sub $0x100, %rbp");
 	
@@ -61,16 +73,23 @@ int f1(void){
 	
 	printf("fopen, after syscall, rbp = %p, rsp = %p\n", (unsigned long *)rbp1, (unsigned long *)getrsp());
 	ret = f2();
-	//printf("ret = %d\n", ret);
+	printf("ret = %d\n", ret);
+#ifdef MEMCLEAR
 	__asm__ volatile (__syscall
                 : "=a" (ret)
                 : "0" (317) : __syscall_clobber );
+#endif
 	//ret = f2();
 	//printf("ret = %d\n", ret);
 	return ret;
 }
 
+void test_call(){
+	int (*func)(void) = f1;
+	(*func)();
+}
+
 int main(void){
-	f1();
+	test_call();
 	printf("finish\n");
 }
