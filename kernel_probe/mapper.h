39a749a287b17b2f1a9c5e0fab0c15af303304e2
qzhang90 <qzhang90@gatech.edu>
2016-07-25 11:58:05 -0400

    Update mapper.h

--- kernel_probe/mapper.h
+++ kernel_probe/mapper.h
@@ -5,22 +5,13 @@ typedef struct item{
         char *func_name;
 	unsigned long ip;
         unsigned long size;
+
+	unsigned long cur;
+	unsigned long cap;
 }item_t;
 
-/*
-typedef struct mdata{
-	unsigned long cap;
-	unsigned long cur;
-}mdata_t;
-
-typedef struct node{
-	item_t mapper;
-	mdata_t md;
-}node_t;
-*/
 item_t *init_mapper(size_t size);
 int insert_mapper(item_t **mapper, char *func_name, unsigned long ip, unsigned long size);
 char *get_func_name(item_t *mapper, unsigned long ip);
 
 #endif
-
