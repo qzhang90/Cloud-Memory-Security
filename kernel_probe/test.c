23199903a24e483150d095c9f319176f5d8c99a3
qzhang90 <qzhang90@gatech.edu>
2016-07-25 11:22:26 -0400

    Update test.c

--- kernel_probe/test.c
+++ kernel_probe/test.c
@@ -6,6 +6,7 @@
 #include <sys/syscall.h>
 #include <sys/user.h>
 #include <unistd.h>
+#include </usr/src/linux-3.14.4-memClear/include/linux/memclear_para.h>
 
 //#define _syscall1(NR) stub_syscall1(NR)
 #define __syscall_clobber "r11","rcx","memory"
@@ -13,39 +14,31 @@
 
 #define MEMCLEAR	1
 long ret;
+para_list_t *para_list;
+
 
 unsigned long long getrsp(){
   __asm__ ("movq %rsp, %rax");
 }
 
 
-int f2(void){
-	int a, b, c;
-	unsigned long i;
-
-	register unsigned long rbp asm("rbp");
-
-	for(i = 0; i < 500; i++){
-		printf("%2lx ", *((char *)rbp + i)&0xFF);
-	}	
-	printf("\n\n\n");
-	
-	a = 1;
-	b = 2;
+void f2(int a, int b){
+	int c;
+	printf("in f2, a = %d, b = %d\n", a, b);
 	c = a + b;
-	return c;
 }
 
 int f1(void){
 	char buff[512];
-	int i, j;
+	int i, j, arg1 = 10, arg2 = 20;
 	for(i = 0; i < 512; i++)
 	    buff[i] = 'a';
 	FILE *f1 = fopen("text.txt", "wb");
 	int r1 = fwrite(buff, 1, 512, f1);
-	
+	para_t *tmp_para_t;
+
 	fclose(f1);
-	int ret, tmp = 123;
+	int ret = 0, tmp = 123;
 	register unsigned long rbp asm("rbp");	
 	register unsigned long rsp asm("rsp");	
 
@@ -60,9 +53,21 @@ int f1(void){
 	
 
 #ifdef MEMCLEAR
+	para_list = malloc(sizeof(para_list_t));
+	para_list->size = 2;
+	para_list->plist = malloc(para_list->size*sizeof(para_t));
+	
+	tmp_para_t = para_list->plist + 0;
+	tmp_para_t->addr = (unsigned long)&arg1;
+	tmp_para_t->size = sizeof(arg1);	
+	
+	tmp_para_t = para_list->plist + 1;
+	tmp_para_t->addr = (unsigned long)&arg2;
+	tmp_para_t->size = (unsigned long)sizeof(arg2);	
+	
 	__asm__ volatile (__syscall
                 : "=a" (ret)
-                : "0" (316), "D" (tmp)
+                : "0" (316), "D" (para_list)
 		: __syscall_clobber );
 #endif
 	/*A malicious user resumes rbp/rsp after the system call*/	
@@ -72,17 +77,20 @@ int f1(void){
 	register unsigned long rsp1 asm("rsp");	
 	
 	printf("fopen, after syscall, rbp = %p, rsp = %p\n", (unsigned long *)rbp1, (unsigned long *)getrsp());
-	ret = f2();
+	f2(arg1, arg2);
 	printf("ret = %d\n", ret);
 #ifdef MEMCLEAR
 	__asm__ volatile (__syscall
                 : "=a" (ret)
-                : "0" (317) : __syscall_clobber );
+                : "0" (317), "D" (para_list) 
+		: __syscall_clobber );
+	free(para_list);
 #endif
 	//ret = f2();
-	//printf("ret = %d\n", ret);
+	printf("f1 finish\n");
 	return ret;
 }
+
 
 void test_call(){
 	int (*func)(void) = f1;
