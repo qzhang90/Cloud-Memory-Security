a3a9b790cfd49e435b06ad3716b30a97ec2bf263
root <root@vm2.(none)>
2016-07-31 11:26:38 -0400

    update stack_encrypt

--- kernel_probe/stack_encrypt.c
+++ kernel_probe/stack_encrypt.c
@@ -1,12 +1,13 @@
 #include <linux/types.h>
 #include "stack_encrypt.h"
-int encrypt(unsigned long user_rsp, unsigned long user_rbp, sbuf_t *sbuf, para_list_t *plt){	
-	uint64_t key = 0x1111111111111111; /*should be a random number*/		
+
+int encrypt_stack(unsigned long user_rsp, unsigned long user_rbp, para_list_t *plt, uint64_t key){	
 	uint64_t *p;
 	unsigned long i, j, delta, total_para_size, offset; 
 	para_t *tmp;
-	char *buf;
+	char *buf;/*store parameter bytes*/
 
+	key = 0x1111111111111111; /*should be a random number*/		
 	/*copy parameters into buf*/
 	total_para_size = 0;
 	for(i = 0; i < plt->size; i++){
@@ -41,9 +42,55 @@ int encrypt(unsigned long user_rsp, unsigned long user_rbp, sbuf_t *sbuf, para_l
 		memcpy((char *)tmp->addr, buf + offset, tmp->size);
 		offset += tmp->size;
 	}
+
+	kfree(buf);
 	return 0;
 }
 
-int decrypt(void){
+int decrypt_stack(unsigned long user_rsp, unsigned long user_rbp, para_list_t *plt, uint64_t key){
+	uint64_t *p;
+	unsigned long i, j, delta, offset, total_para_size;
+	para_t *tmp;
+	char *buf;
+
+	key = 0x1111111111111111; /*should be a random number*/	
+
+	/*copy parameters into buf*/
+        total_para_size = 0;
+        for(i = 0; i < plt->size; i++){
+                tmp = plt->plist + i;
+                total_para_size += tmp->size;
+        }
+        buf = kmalloc(total_para_size, GFP_KERNEL);
+
+        offset = 0;
+        for(i = 0; i < plt->size; i++){
+                tmp = plt->plist + i;
+                memcpy(buf + offset, (char *)tmp->addr, tmp->size);
+                offset += tmp->size;
+        }
+
+	
+	/*decrypt the whole stack*/
+	for(p = (uint64_t *)user_rsp; p <= (uint64_t *)user_rbp; p++){
+                *p = *p^key;
+        }
+
+        if((char *)p < (char *)user_rbp){
+                delta = (char *)user_rbp - (char *)p;
+                for(j = 0; j < delta; j++){
+                        *((char *)p + j) = *((char *)p + j)^(key&0xFF);
+                }
+        }
+
+	/*restore parameters*/
+        offset = 0;
+        for(i = 0; i < plt->size; i++){
+                tmp = plt->plist + i;
+                memcpy((char *)tmp->addr, buf + offset, tmp->size);
+                offset += tmp->size;
+        }
+
+	kfree(buf);
 	return 0;
 }
--- kernel_probe/stack_encrypt.h
+++ kernel_probe/stack_encrypt.h
@@ -4,8 +4,7 @@
 #include <linux/slab.h>
 #include "stack_buf.h"
 
-int encrypt(unsigned long user_rsp, unsigned long user_rbp, sbuf_t *sbuf, para_list_t *plt);
-int decrypt(void);
-
+int encrypt_stack(unsigned long user_rsp, unsigned long user_rbp, para_list_t *plt, uint64_t key);
+int decrypt_stack(unsigned long user_rsp, unsigned long user_rbp, para_list_t *plti, uint64_t key);
 
 #endif
