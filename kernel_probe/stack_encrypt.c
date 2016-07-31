b7b50a5a40aaedeead5b2545cf76a88d6ff7531f
qzhang90 <qzhang90@gatech.edu>
2016-07-31 10:30:43 -0400

    Create stack_encrypt.c

---
+++ kernel_probe/stack_encrypt.c
@@ -0,0 +1,49 @@
+#include <linux/types.h>
+#include "encrypt.h"
+int encrypt(unsigned long user_rsp, unsigned long user_rbp, sbuf_t *sbuf, para_list_t *plt){	
+	uint64_t key = 0x1111111111111111; /*should be a random number*/		
+	uint64_t *p;
+	unsigned long i, j, delta, total_para_size, offset; 
+	para_t *tmp;
+	char *buf;
+
+	/*copy parameters into buf*/
+	total_para_size = 0;
+	for(i = 0; i < plt->size; i++){
+		tmp = plt->plist + i;
+		total_para_size += tmp->size;	
+	}
+	buf = kmalloc(total_para_size, GFP_KERNEL);
+	
+	offset = 0;	
+	for(i = 0; i < plt->size; i++){
+		tmp = plt->plist + i;
+		memcpy(buf + offset, (char *)tmp->addr, tmp->size);
+		offset += tmp->size;
+	}
+	
+	/*encrypt the whole stack*/
+	for(p = (uint64_t *)user_rsp; p <= (uint64_t *)user_rbp; p++){
+		*p = *p^key;
+	}
+
+	if((char *)p < (char *)user_rbp){
+		delta = (char *)user_rbp - (char *)p;
+		for(j = 0; j < delta; j++){
+			*((char *)p + j) = *((char *)p + j)^(key&0xFF);	
+		}
+	}
+	
+	/*restore parameters*/	
+	offset = 0;
+	for(i = 0; i < plt->size; i++){
+		tmp = plt->plist + i;
+		memcpy((char *)tmp->addr, buf + offset, tmp->size);
+		offset += tmp->size;
+	}
+	return 0;
+}
+
+int decrypt(void){
+	return 0;
+}
